833729ab5d555866e2cb790bccbfe93c7f345e13
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-12 16:43:36 +0200

    feat: add home end point

---
+++ src/main/java/com/exercise/oauth_github/controller/MyController.java
@@ -0,0 +1,26 @@
+package com.exercise.oauth_github.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    @GetMapping("/home")
+    public String home(Model model, @AuthenticationPrincipal OAuth2User oAuth2User) {
+        model.addAttribute("user", oAuth2User.getAttribute("login"));
+        model.addAttribute("profileLink", oAuth2User.getAttribute("html_url"));
+        model.addAttribute("repos", oAuth2User.getAttribute("repos_url"));
+        return "home";
+    }
+
+
+}
