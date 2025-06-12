8345e970e400e872cdcad0e3c2393293fde1289b
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-12 16:19:10 +0200

    chore: set up project with the required dependencies

---
+++ src/main/java/com/exercise/oauth_github/OauthGithubApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.oauth_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/oauth_github/OauthGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.oauth_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
